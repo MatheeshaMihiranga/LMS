79a22d32dafa82441c4100010771f0c4b1c2afb1
Matheesha Mihiranga <133772302+MatheeshaMihiranga@users.noreply.github.com>
2025-03-25 01:57:15 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Routes, Route, useMatch } from "react-router-dom";
+import Home from "./pages/student/Home";
+import CoursesList from "./pages/student/CoursesList";
+import CourseDetails from "./pages/student/CourseDetails";
+import Player from "./pages/student/Player";
+import Loading from "./components/student/Loading";
+import Educator from "./pages/educator/Educator";
+import DashBoard from "./pages/educator/DashBoard";
+import AddCourse from "./pages/educator/AddCourse";
+import MyCourse from "./pages/educator/MyCourse";
+import StudentsEnrolled from "./pages/educator/StudentsEnrolled";
+import Navbar from "./components/student/Navbar";
+import MyEnrollment from "./pages/student/MyEnrollment";
+import "quill/dist/quill.snow.css";
+
+const App = () => {
+  const isEducatorRoute = useMatch("/educator/*");
+
+  return (
+    <div className="text-defult min-h-screen bg-white">
+      {!isEducatorRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollment />} />
+        <Route path="/player/:couresId" element={<Player />} />
+        <Route path="/loading/:path" element={<Loading />} />
+        <Route path="/educator" element={<Educator />}>
+          <Route path="/educator" element={<DashBoard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-course" element={<MyCourse />} />
+          <Route path="students-enrolled" element={<StudentsEnrolled />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/mongodb.js";
+import { clerkWebhook } from "./controllers/webhooks.js";
+
+//initialize express
+const app = express();
+
+//Connect to database
+await connectDB();
+
+//middleware
+app.use(cors());
+
+//routes
+app.get("/", (req, res) => res.send("API Working"));
+app.post("/clerk", express.json(), clerkWebhook);
+
+//port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
